acf06f233a16542580f52d77f5b9f366364c66e9
Luipy56 <yoelberjaga@gmail.com>
2025-08-05 17:55:07 +0200

    Actualizacion 18:00

--- scenes/laptopButton.gd
+++ scenes/laptopButton.gd
@@ -3,6 +3,9 @@ extends Area2D
 @onready var laptopButton = self
 @onready var openingLaptopAnimation = $"../openingLaptop"
 @onready var openingLaptopSound = $"../openingLaptopSound"
+@onready var cameraUI = $"../camerasNode/camUI"
+@onready var cam1A = $"../camerasNode/cameraSystem/Cam1A"
+@onready var cam1ABackground = $"../camerasNode/cameraSystem/Cam1A/Background"
 
 func _ready() -> void:
 	openingLaptopAnimation.visible = false
@@ -16,3 +19,6 @@ func _ready() -> void:
 	
 func _on_animation_finished() -> void:
 	openingLaptopAnimation.visible = false
+	cameraUI.visible = true
+	cam1A.visible = true
+	cam1ABackground.visible = true
--- scripts/office/cameraLogic/cameras_node.gd
+++ scripts/office/cameraLogic/cameras_node.gd
@@ -1,48 +1,77 @@
 extends Node2D
 
-@onready var spriteCam = $cam
+#@onready var spriteCam = $cam
 var normalCamsList = []
 var animatronicCamsList = []
 
+@onready var cameraSystem = $"cameraSystem"
 
+#var dic = {
+	#"cam1A":{
+		#"normal":null,
+		#"bonnie":null,
+		#"chica":null
+	#},
+	#"cam1Adata":null,
+	#"cam1B":{
+		#"normal":null,
+		#"bonnie1":null,
+		#"bonnie2":null,
+		#"chica1":null,
+		#"chica2":null,
+		#"freddy":null
+	#},
+	#"cam1Bdata":null,
+#}
 
 func _ready() -> void:
-	spriteCam.texture = null
-	
-	var fileNames = [
-		"cam1A",
-		"cam1B",
-		"cam1C",
-		"cam2A",
-		"cam2B",
-		"cam3",
-		"cam4A",
-		"cam4B",
-		"cam5",
-		"cam6",
-		"cam7"
-	]
+	#spriteCam.texture = null
+	cameraSystem.visible = true
+	#var fileNames = [
+		#"cam1A",
+		#"cam1B",
+		#"cam1C",
+		#"cam2A",
+		#"cam2B",
+		#"cam3",
+		#"cam4A",
+		#"cam4B",
+		#"cam5",
+		#"cam6",
+		#"cam7"
+	#]
+	#var fileAnimatronicNames = [
+		#"cam1A",
+		#"cam1B",
+		#"cam1C",
+		#"cam2A",
+		#"cam2B",
+		#"cam3",
+		#"cam4A",
+		#"cam4B",
+		#"cam5",
+		#"cam6",
+		#"cam7"
+	#]
 
-	# Construir rutas completas y cargar texturas
-	for name in fileNames:
-		var ruta = "res://assets/img/office/cameras/" + name +".png"
-		var textura = load(ruta)
-		if not textura:
-			push_warning("No se pudo cargar: " + ruta)
-			continue
-			
-		if nombre.begins_with("freddyMenuStatic"):
-			freddyMenu.append(textura)
-			
-		elif nombre.begins_with("freddyMenuWeird1Static") or nombre.begins_with("freddyMenuWeird2Static") or nombre.begins_with("freddyMenuWeird3Static"):
-			freddyMenuWeird.append(textura)
-			
-		elif nombre == "freddyMenu.png":
-			freddyMenu.append(textura)
-	
+	#for name in fileNames:
+		#var ruta = "res://assets/img/office/cameras/"+ name +".png"
+		#var textura = load(ruta)
+		#if not textura:
+			#push_warning("No se pudo cargar: " + ruta)
+			#continue
+		#normalCamsList.append(textura)
+		
+	#for name in fileAnimatronicNames:
+		#var ruta = "res://assets/img/office/cameras/" + name +".png"
+		#var textura = load(ruta)
+		#if not textura:
+			#push_warning("No se pudo cargar: " + ruta)
+			#continue
+		#normalCamsList.append(textura)
 
 func changeCamera(id):
 	
-	
+	print(id)
 	
 	pass
